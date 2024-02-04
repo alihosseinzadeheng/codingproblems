47f67fb1329cfc21762d975e4249353809b8d221
Ali Hosseinzadeh <hoseinzadeh@jibit.ir>
2024-02-04 15:15:55 +0330

    Update spring boot version

--- src/main/java/ir/khodam/CountingDuplicateCharacters.java
+++ src/main/java/ir/khodam/CountingDuplicateCharacters.java
@@ -4,13 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CountingDuplicateCharacters {
-    public static void main(String[] args) {
+    public static void d(String[] args) {
         String str = "";
         Map<Character, Integer> result = new HashMap<>();
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
         }
+
+        String strMaskan="Hibabyjan0123456";
+        System.out.println(strMaskan.substring(strMaskan.length()-13, 15));
     }
 }
 
